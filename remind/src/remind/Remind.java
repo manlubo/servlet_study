3e7acc354c400b55ed5d07ce2cfacdcbb414163a
manlubo <manlubo11@gmail.com>
2025-06-05 11:39:03 +0900

    서블릿 리뷰

---
+++ remind/src/remind/Remind.java
@@ -0,0 +1,45 @@
+package remind;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Remind {
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<String>(List.of("가","A","10","나","가","A","10","나"));
+		System.out.println(list.subList(3, 5));
+		String str = String.join("-", list);
+		System.out.println(str);
+		str.split("-");
+		List<String> list2 = Arrays.asList(str.split("-"));
+		
+		list.addAll(list2);
+		System.out.println(list);
+		
+		Stack<Integer> stack = new Stack<>();
+		stack.push(10);
+		stack.push(20);
+		stack.push(30);
+		
+		System.out.println(stack);
+		
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		
+		Queue<Integer> queue = new LinkedList<Integer>();
+		
+		queue.add(10);
+		queue.add(20);
+		queue.add(30);
+		
+		System.out.println(queue);
+		
+		System.out.println(queue.poll());
+		System.out.println(queue.poll());
+		System.out.println(queue.poll());
+ 	}
+}
