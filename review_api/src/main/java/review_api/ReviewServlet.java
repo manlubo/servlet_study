355d33e655e97e474ee3c81617363b4ccd286529
manlubo <manlubo11@gmail.com>
2025-06-09 13:13:52 +0900

    리뷰 완성

--- review_api/src/main/java/review_api/ReviewServlet.java
+++ review_api/src/main/java/review_api/ReviewServlet.java
@@ -1,6 +1,7 @@
 package review_api;
 
 import java.io.IOException;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -16,22 +17,21 @@ import service.ReviewService;
 
 @WebServlet ("/review/*")
 public class ReviewServlet extends HttpServlet{
-
+	public static final String ID = "/review/";
+	
+	private String getURI(HttpServletRequest req) {
+		String uri = req.getRequestURI();
+		uri = uri.substring(uri.indexOf(ID) + ID.length());
+		return uri;
+	}
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-//		System.out.println("health check");
-//		uri
-//		/review/list
-		
-//		System.out.println(req.getContextPath());
-//		System.out.println(req.getRequestURI());
-		
-		String uri = req.getRequestURI();
-		uri = uri.substring(uri.indexOf("review/") + "review/".length());
+		String uri = getURI(req);
 		ReviewService service = new ReviewService();
 		Gson gson = new Gson(); 
 		String ret = "";
-		if(uri.startsWith("list")) { // 목록조회
+		if(uri.startsWith("list") || uri.equals("*")) { // 목록조회
 			ret = gson.toJson(service.list());
 		}
 		else { // 단일조회
@@ -40,14 +40,16 @@ public class ReviewServlet extends HttpServlet{
 		System.out.println(ret);
 		resp.setContentType("application/json; charset=utf-8");
 		resp.getWriter().print(ret);
-		// json
-		
-		//		/review/1
 	}
 
 	@Override
 	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String uri = getURI(req);
+		Long rno = Long.valueOf(uri);
+		boolean result = new ReviewService().remove(rno) > 0; 
 		
+		resp.setContentType("application/json; charset=utf-8");
+		resp.getWriter().print(new Gson().toJson(Map.of("result", result)));
 	}
 
 	@Override
@@ -56,11 +58,24 @@ public class ReviewServlet extends HttpServlet{
 		String ret = String.join("",req.getReader().lines().toList());
 		Review review = new Gson().fromJson(ret, Review.class);
 		new ReviewService().register(review);
+		
+		resp.setContentType("application/json; charset=utf-8");
+		resp.getWriter().print(new Gson().toJson(Map.of("result", true)));
 	}
 
 	@Override
 	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String uri = getURI(req);
+		Long rno = Long.valueOf(uri);
 		
+		req.setCharacterEncoding("utf-8");
+		String ret = String.join("",req.getReader().lines().toList());
+		Review review = new Gson().fromJson(ret, Review.class);
+		
+		boolean result = new ReviewService().modify(review) > 0; 
+		
+		resp.setContentType("application/json; charset=utf-8");
+		resp.getWriter().print(new Gson().toJson(Map.of("result", result)));
 	}
 	
 	
